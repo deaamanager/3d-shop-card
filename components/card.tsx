e0801e8bbaf1ecee925aa0a24cf29bb69ec824d3
dea <108593938+deaamanger@users.noreply.github.com>
2023-05-21 04:48:56 +0200

    letzte add

--- components/card.tsx
+++ components/card.tsx
@@ -8,7 +8,7 @@ const LaptopDynamic = dynamic(() =>import("./Laptop"));
 
 function card() {
   return (
-    <div className="absolute flex  items-center  justify-center  z-40  w-full  h-screen" >
+    <div className="absolute flex items-center justify-center z-40 w-full h-screen" >
     <Canvas shadows dpr={[1, 1]} camera={{position:[0,0,4],fov:10 }}>
     <ambientLight intensity={0.7} />
      <spotLight intensity={0.7} angle={0.7} penumbra={8} position={[10,15,16]}  castshadow />
